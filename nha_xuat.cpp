a8a197391268793ab28faf67c6ea873a96ff1ac8
Hoa Tử Đăng <147475712+THDANG791407@users.noreply.github.com>
2024-05-30 18:36:41 +0700

    //thêm phương thức khởi tạo
    thêm phương thức khởi tạo đối số và phương thức khởi tạo sao chép, thêm điểm tb

--- nha_xuat.cpp
+++ nha_xuat.cpp
@@ -5,30 +5,56 @@ using namespace std;
 class SinhVien{
 	string hoten;
 	int namsinh;
+	float *diemtb;
 	public:
+		SinhVien(string ht, int ns, float diem); //Phuong thuc khoi tao co doi so
+		SinhVien(SinhVien &sv);
+		~SinhVien();
 		void nhap();
 		void hienthi();
 };
-void SinhVien::	nhap()
+/*void SinhVien::nhap()
 {
 	cout <<"nhap ho va ten: ";
 	fflush (stdin);
 	getline(cin,this->	hoten);
 	cout <<endl;
 	cout <<"nhap nam sinh: ";
+	fflush (stdin);
 	cin>> this->namsinh;
-}
+	cout <<endl;
+	cout <<"nhap diem trung binh: ";
+	cin>> *(this->diemtb);
+}*/
 void SinhVien::hienthi()
 {
 	cout <<"________________________"<<endl;
-	cout <<hoten<<" sinh nam:"<<namsinh<<endl;
+	cout <<hoten<<" sinh nam:"<<namsinh<<" diem tb:"<<*(this->diemtb)<<endl;
+}
+SinhVien::	SinhVien(string ht, int ns, float diem)
+{
+	this->hoten = ht;
+	this->namsinh = ns;
+	this->diemtb = new float; // cap phat vung nho cho con tro diemtb
+	*(this->diemtb)= diem;
+}
+SinhVien::SinhVien(SinhVien &sv)
+{
+	this->hoten= sv.hoten;
+	this->namsinh= sv.namsinh;
+	this->diemtb = new float;
+	this->diemtb= sv.diemtb;
+}
+SinhVien::~SinhVien()
+{
+	delete (this->diemtb);
 }
 
 int main()
 {
-	SinhVien sv;
-	sv.	nhap();
-	sv.hienthi();
+	SinhVien sv("Huong Nguyen",2000,9.0);
+	SinhVien sv1(sv);
+	sv1.hienthi();
 	
 	return 0;
 }
